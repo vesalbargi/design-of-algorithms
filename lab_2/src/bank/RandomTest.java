d58b4edd29e4da1607f232ce1ac0f0a86714d337
Vesal Bargi <vesalbargi81@gmail.com>
2024-10-17 18:16:33 +0330

    fix: unify accounts for both banks

--- lab_2/src/bank/RandomTest.java
+++ lab_2/src/bank/RandomTest.java
@@ -6,25 +6,42 @@ import java.util.HashMap;
 
 public class RandomTest {
     private static final int TEST_NUM = 1000;
+    private static final int TEST_REP = 10;
     private static final String ADD = "add";
     private static final String FIND = "find";
     private static final String CITY = "city";
     private static final String RANGE = "range";
+    private ArrayList<Account> accounts;
+
+    public RandomTest() {
+        this.accounts = new ArrayList<>();
+    }
 
     public static void main(String[] args) {
         Bank bank = new Bank("Test Bank");
         Bank2 bank2 = new Bank2("Test Bank2");
         RandomTest randomTest = new RandomTest();
-        randomTest.testBank(bank);
-        randomTest.testBank2(bank2);
+        randomTest.generateAccounts();
+        randomTest.timeTest(bank, bank2);
+    }
+
+    public void timeTest(Bank bank, Bank2 bank2) {
+        for (int i = 0; i < TEST_REP; i++) {
+            testBank(bank);
+            testBank2(bank2);
+        }
+    }
+
+    public void generateAccounts() {
+        for (int i = 0; i < TEST_NUM; i++) {
+            accounts.add(createRandomAccounts());
+        }
     }
 
     public void testBank(Bank bank) {
         long begin = getTime();
-        for (int i = 0; i < TEST_NUM; i++) {
-            Account acc = createRandomAccounts();
+        for (Account acc : accounts) {
             bank.addAccount(acc);
-            // addAccount(bank, acc);
         }
         long finish = getTime();
         long addAccountTime = calcTime(begin, finish);
@@ -32,7 +49,6 @@ public class RandomTest {
         begin = getTime();
         for (int i = 1; i <= TEST_NUM; i++) {
             bank.findAccount(i);
-            // findAccount(bank, i);
         }
         finish = getTime();
         long findAccountTime = calcTime(begin, finish);
@@ -41,7 +57,6 @@ public class RandomTest {
         ArrayList<String> cities = bank.populateDistinctCityList();
         ArrayList<Double> balances = bank.getTotalBalancePerCity(cities);
         ArrayList<Integer> counts = bank.getTotalCountPerCity(cities);
-        // bank.reportTotalPerCity(cities, counts, balances);
         finish = getTime();
         long cityAggregationTime = calcTime(begin, finish);
 
@@ -49,11 +64,10 @@ public class RandomTest {
         Integer[] r = { 1, 1000, 10000, 100000, 10000000 };
         ArrayList<Integer> ranges = new ArrayList<Integer>(Arrays.asList(r));
         ArrayList<Integer> countsPerRange = bank.getTotalCountPerRange(ranges);
-        // bank.reportRanges(ranges, countsPerRange);
         finish = getTime();
         long rangeAggregationTime = calcTime(begin, finish);
 
-        System.out.println("-----------------Bank 1-----------------");
+        System.out.println("-------------------Bank---------------------");
         printTime(addAccountTime, ADD);
         printTime(findAccountTime, FIND);
         printTime(cityAggregationTime, CITY);
@@ -62,10 +76,8 @@ public class RandomTest {
 
     public void testBank2(Bank2 bank2) {
         long begin = getTime();
-        for (int i = 0; i < TEST_NUM; i++) {
-            Account acc = createRandomAccounts();
+        for (Account acc : accounts) {
             bank2.addAccount(acc);
-            // addAccount(bank2, acc);
         }
         long finish = getTime();
         long addAccountTime = calcTime(begin, finish);
@@ -73,7 +85,6 @@ public class RandomTest {
         begin = getTime();
         for (int i = 1; i <= TEST_NUM; i++) {
             bank2.findAccount(i);
-            // findAccount(bank2, i);
         }
         finish = getTime();
         long findAccountTime = calcTime(begin, finish);
@@ -81,7 +92,6 @@ public class RandomTest {
         begin = getTime();
         HashMap<String, Double> cities2 = bank2.getTotalBalancePerCity();
         HashMap<String, Integer> counts2 = bank2.getTotalCountPerCity();
-        // bank2.reportCity(cities2, counts2);
         finish = getTime();
         long cityAggregationTime = calcTime(begin, finish);
 
@@ -89,11 +99,10 @@ public class RandomTest {
         Integer[] r = { 1, 1000, 10000, 100000, 10000000 };
         ArrayList<Integer> ranges = new ArrayList<Integer>(Arrays.asList(r));
         HashMap<Integer, Integer> countsPerRange1 = bank2.getTotalCountPerRange(ranges);
-        // bank2.reportRanges(ranges, countsPerRange1);
         finish = getTime();
         long rangeAggregationTime = calcTime(begin, finish);
 
-        System.out.println("-----------------Bank 2-----------------");
+        System.out.println("-------------------Bank 2-------------------");
         printTime(addAccountTime, ADD);
         printTime(findAccountTime, FIND);
         printTime(cityAggregationTime, CITY);
@@ -127,16 +136,16 @@ public class RandomTest {
     public void printTime(long time, String operation) {
         switch (operation) {
             case "add":
-                System.out.println("Added " + TEST_NUM + " accounts in " + time + " nanoseconds");
+                System.out.printf("%-20s %10d accounts in: %10d nanoseconds%n", "Added", TEST_NUM, time);
                 break;
             case "find":
-                System.out.println("Found " + TEST_NUM + " accounts in " + time + " nanoseconds");
+                System.out.printf("%-20s %10d accounts in: %10d nanoseconds%n", "Found", TEST_NUM, time);
                 break;
             case "city":
-                System.out.println(TEST_NUM + " accounts city aggregation completed in: " + time + " nanoseconds");
+                System.out.printf("%-20s %10d accounts in: %10d nanoseconds%n", "City aggregation", TEST_NUM, time);
                 break;
             case "range":
-                System.out.println(TEST_NUM + " accounts range aggregation completed in: " + time + " nanoseconds");
+                System.out.printf("%-20s %10d accounts in: %10d nanoseconds%n", "Range aggregation", TEST_NUM, time);
                 break;
             default:
                 System.out.println("Unsupported operation");
